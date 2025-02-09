9e88e232b45845f5c569389ed3a9b9a93a09fbba
truefuego <ankitsharma15092003@gmail.com>
2025-02-09 12:34:54 +0530

    Add - Fonts, routing and tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import HomePage from './pages/HomePage'
+import NotFoundPage from './pages/NotFoundPage'
+import PersonalProjectsPage from './pages/PersonalProjectsPage'
+import ProjectPage from './pages/ProjectPage'
+import AboutMePage from './pages/AboutMePage'
+import GalleryPage from './pages/GalleryPage'
 
 function App() {
   return (
-    <>
-      hello
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/works" element={<PersonalProjectsPage />} />
+        <Route path="/project/:id" element={<ProjectPage />} />
+        <Route path="/about-me" element={<AboutMePage />} />
+        <Route path="/gallery" element={<GalleryPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/common/CustomLoader.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CustomLoader:React.FC = () => {
+  return (
+    <div>CustomLoader</div>
+  )
+}
+
+export default CustomLoader
