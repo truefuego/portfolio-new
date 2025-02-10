cfc1d6c44b7bfd1c562349c5e775cc6bf82acfd5
truefuego <ankitsharma15092003@gmail.com>
2025-02-10 11:30:37 +0530

    Add - Complete Desktop Navbar

--- src/components/common/CustomLinkText.tsx
+++ src/components/common/CustomLinkText.tsx
@@ -1,35 +1,29 @@
-import React from "react";
+import React from 'react'
 import { ICustomText } from "./type";
-import { useNavigate } from "react-router-dom";
 
-const CustomText: React.FC<ICustomText> = ({
+const CustomLinkText: React.FC<ICustomText> = ({
   title,
   classes,
   fontFamily,
   fontSize,
   fontColor,
   link,
-  openNewPage,
-  isClickable = true
+  isClickable = true,
+  href
 }) => {
-  const navigate = useNavigate();
   return (
-    <p
+    <a
       className={`text-${fontColor ? fontColor : "primary-text"} 
         ${fontFamily ? fontFamily : "funnel-sans"} 
         text-${fontSize} ${classes}
         ${link ? "cursor-pointer" : ""}
         ${isClickable ? '' : 'pointer-events-none'}
     `}
-      onClick={() => {
-        if (link) {
-          return openNewPage ? window.open(link, "_blank") : navigate(link);
-        }
-      }}
+      href={href}
     >
       {title}
-    </p>
-  );
+    </a>
+  )
 };
 
-export default CustomText;
+export default CustomLinkText;
--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import CustomText from './CustomTextNormal'
+import CustomLinkText from './CustomLinkText'
 
 const NavBar:React.FC = () => {
   return (
-    <div className='flex flex-row justify-between p-8 w-[85%]'>
+    <div className='flex flex-row justify-between p-8 default-width'>
       <CustomText title='ANKIT' fontFamily='climate-crisis' isClickable={false}/>
       <div className='flex flex-row gap-16'>
         <CustomText title='ABOUT' link='/about-me'/>
-        <CustomText title='CONTACT ME' href='mailto:ankitsharma9152003@gmail.com'/>
+        <CustomLinkText title='CONTACT ME' href='mailto:ankitsharma9152003@gmail.com'/>
         <CustomText title='MY CV' openNewPage link='https://docs.google.com/document/d/1jDhy-1AAO8UCjjekwRIJSBac0A1UYu1V/edit?usp=sharing&ouid=102246377516776370837&rtpof=true&sd=true'/>
       </div>
     </div>
