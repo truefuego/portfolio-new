14d96be6d13897d9bb7e995839d698b9570cb657
truefuego <ankitsharma15092003@gmail.com>
2025-02-16 00:41:53 +0530

    Fix - slider

--- src/components/InfiniteSlider.tsx
+++ src/components/InfiniteSlider.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import CustomImage from './common/CustomImage'
 import DuskImage from '../assets/images/dusk-image.webp';
 import UpTodoImage from '../assets/images/uptodo-image.png';
@@ -15,26 +15,26 @@ const InfiniteSlider = () => {
     useEffect(() => {
         let finalPosition = -80;
         if(innerWidth >= 1536) {
-            finalPosition -= 64*4*5;
+            finalPosition -= 52*4*aboutMeImages.length;
         } else if(innerWidth >= 640) {
-            finalPosition -= 48*4*5;
+            finalPosition -= 48*4*aboutMeImages.length;
         } else {
-            finalPosition -= 24*4*5;
+            finalPosition -= 24*4*aboutMeImages.length;
         }
         const controls = animate(xTranslation, [0, finalPosition], {
             ease: 'linear',
-            duration: 10,
+            duration: 25,
             repeat: Infinity,
             repeatLoop: 'loop',
             repeatDelay: 0 
         });
         return controls.stop;
-    }, [xTranslation])
+    }, [aboutMeImages.length, xTranslation])
 
   return (
     <motion.div className='flex gap-4 w-fill' style={{ x: xTranslation }} >
-        {aboutMeImages.map((item, index) => <CustomImage key={index} src={item} alt="image" classes='w-24 sm:w-48 2xl:w-64 rounded-xl' />)}
-        {aboutMeImages.map((item, index) => <CustomImage key={index} src={item} alt="image" classes='w-24 sm:w-48 2xl:w-64 rounded-xl' />)}
+        {aboutMeImages.map((item, index) => <CustomImage key={index} src={item} alt="image" classes='w-24 sm:w-48 2xl:w-52 rounded-xl' />)}
+        {aboutMeImages.map((item, index) => <CustomImage key={index} src={item} alt="image" classes='w-24 sm:w-48 2xl:w-52 rounded-xl' />)}
     </motion.div>
   )
 }
