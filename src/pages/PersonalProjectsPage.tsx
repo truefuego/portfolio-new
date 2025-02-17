575a28b8600c139cba6860e71be7ba636419f694
truefuego <ankitsharma15092003@gmail.com>
2025-02-17 14:49:03 +0530

    Add - project view components

--- src/pages/PersonalProjectsPage.tsx
+++ src/pages/PersonalProjectsPage.tsx
@@ -1,21 +1,39 @@
-import React, { useState } from 'react'
-import ScreenWrapper from '../components/ScreenWrapper';
-import CustomText from '../components/common/CustomTextNormal';
-import { viewStyleFilterTypes, workTypeFilterTypes } from '../constants/constants';
-import ProjectsFilterButtonSection from '../components/work/ProjectsFilterButtonSection';
+import React, { useState } from "react";
+import ScreenWrapper from "../components/ScreenWrapper";
+import CustomText from "../components/common/CustomTextNormal";
+import {
+  viewStyleFilterTypes,
+  workTypeFilterTypes,
+} from "../constants/constants";
+import ProjectsFilterButtonSection from "../components/work/ProjectsFilterButtonSection";
+import ProjectsTableView from "../components/work/ProjectsTableView";
+import ProjectsGridView from "../components/work/ProjectsGridView";
 
-const PersonalProjectsPage:React.FC = () => {
-  const [selectedFilter, setSelectedFilter] = useState<string>(workTypeFilterTypes.ALL);
-  const [selectedViewStyle, setSelectedViewStyle] = useState<string>(viewStyleFilterTypes.FLEX);
+const PersonalProjectsPage: React.FC = () => {
+  const [selectedFilter, setSelectedFilter] = useState<string>(
+    workTypeFilterTypes.ALL
+  );
+  const [selectedViewStyle, setSelectedViewStyle] = useState<string>(
+    viewStyleFilterTypes.FLEX
+  );
 
   return (
     <ScreenWrapper>
-      <div className='w-[65%] mt-48'>
-        <CustomText title='Creating next level digital products' classes='w-[70%] text-7xl' />
-        <ProjectsFilterButtonSection selectedFilter={selectedFilter} selectedViewStyle={selectedViewStyle} setSelectedFilter={setSelectedFilter} setSelectedViewStyle={setSelectedViewStyle}/> 
+      <div className="w-[65%] mt-48">
+        <CustomText
+          title="Creating next level digital products"
+          classes="w-[70%] text-7xl"
+        />
+        <ProjectsFilterButtonSection
+          selectedFilter={selectedFilter}
+          selectedViewStyle={selectedViewStyle}
+          setSelectedFilter={setSelectedFilter}
+          setSelectedViewStyle={setSelectedViewStyle}
+        />
+        {selectedViewStyle === viewStyleFilterTypes.FLEX ? (<ProjectsTableView />) : (<ProjectsGridView />)}
       </div>
     </ScreenWrapper>
-  )
-}
+  );
+};
 
 export default PersonalProjectsPage;
